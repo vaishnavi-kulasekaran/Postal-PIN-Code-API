fe976f2d16c17ca8c369fb5ca678348a825acf80
vaishnavi-kulasekaran <72563225+vaishnavi-kulasekaran@users.noreply.github.com>
2021-07-14 12:06:28 +0530

    Update pincode.php

--- pincode.php
+++ pincode.php
@@ -18,61 +18,60 @@
    </form>
    <?php
         if(isset($_POST['submit']))
-        {
-            $pincode = $_POST['pincode'];
-            $site = "https://api.postalpincode.in/pincode/";
-            $url = $site . $pincode;
-            $cSession = curl_init();
-            curl_setopt($cSession, CURLOPT_URL, $url);
-            curl_setopt($cSession, CURLOPT_RETURNTRANSFER, true);
-            $res = curl_exec($cSession);
-            $data = json_decode($res, true);
-            $values = 0; 
-            $values = count($data['0']['PostOffice']);
-            ?>
-            <table>
-               <tr>
-                   <th>Branch Name</th>
-                   <th>Branch Type</th>
-                   <th>Details</th>
-               </tr>
-               <tr>
-                    <br>
-                    <td>
-                    <?php
-                    for($x=0; $x < $values; $x++)
+        {   
+            if(empty($_POST['pincode']))
+            {
+                echo "Please Enter the Pincode!!";
+            }
+            else
+            {
+                $pincode = $_POST['pincode'];
+                if(strlen($pincode) == '6')
+                {
+                    $site = "https://api.postalpincode.in/pincode/";
+                    $url = $site . $pincode;
+                    $cSession = curl_init();
+                    curl_setopt($cSession, CURLOPT_URL, $url);
+                    curl_setopt($cSession, CURLOPT_RETURNTRANSFER, true);
+                    $res = curl_exec($cSession);
+                    $data = json_decode($res, true);
+                    if($data['0']['PostOffice'] !== null)
                     {
-                        print_r($data['0']['PostOffice'][$x]['Name']);
-                        echo "<br/ >";
-                    }
-                    ?>
-                    </td>
-                    <td>
-                    <?php
-                    for($x=0; $x < $values; $x++)
-                    {
-                        print_r($data['0']['PostOffice'][$x]['BranchType']);
-                        echo "<br/ >";
-                    }
-                    ?>
-                    </td>
-                    <td>
-                       <?php
+                        $values = 0; 
+                        $values = count($data['0']['PostOffice']);
+                        ?>
+                        <table>
+                           <tr>
+                               <th>Branch Name</th>
+                               <th>Branch Type</th>
+                               <th>Details</th>
+                               <br>
+                            </tr>
+                            <?php
                             for($x=0; $x < $values; $x++)
                             {
-                                ?>
-                               <a href="<?php echo "details.php"; ?>?branch_name=<?php print_r($data['0']['PostOffice'][$x]['Name']);?>&pincode=<?php echo $pincode; ?>">
-                               <?php
-                               echo "#";    
-                               echo "<br/ >";
+                            ?>
+                                <tr>
+                                    <td><?php print_r($data['0']['PostOffice'][$x]['Name']); ?></td>
+                                    <td><?php print_r($data['0']['PostOffice'][$x]['BranchType']); ?></td>
+                                    <td><a href="<?php echo "details.php"; ?>?branch_name=<?php print_r($data['0']['PostOffice'][$x]['Name']);?>&pincode=<?php echo $pincode; ?>"># <br></a></td>
+                                </tr>
+                            <?php
                             }
-                        ?>
-                        </a>
-                    </td>
-                    
-                </tr>
-            </table>        
-            <?php
+                            ?>
+                        </table>        
+                        <?php
+                    }
+                    else
+                    {
+                        echo "Pincode is not correct";
+                    }
+                }
+                else
+                    {
+                        echo"Please Enter only six digits!!";
+                    }
+            }
         }
        ?>
    </body>
